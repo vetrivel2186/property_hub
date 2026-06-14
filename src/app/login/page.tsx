fbefa7d825603b3e9f1cf516848814f2a6632a80
vetri <vetrivel2186@gmail.com>
2026-06-14 12:03:31 +0530

    initial commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Login() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white sm:items-start">
+       
+      </main>
+    </div>
+  );
+}
